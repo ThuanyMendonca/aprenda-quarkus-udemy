00a72fda6ae572848014e356de814fa9a6009cb1
Thuany Mendonca <thuany1997@hotmail.com>
2026-06-22 21:27:50 -0300

    Adicionando a tabela followers

--- db/db.sql
+++ db/db.sql
@@ -1,14 +1,20 @@
 create database quarkus-social;
 
 create table users (
-	id bigserial not null primary key;
-	name varchar(100) not null;
+	id bigserial not null primary key,
+	name varchar(100) not null,
 	age integer not null
 )
 
 create table posts (
-	id bigserial not null primary key;
+	id bigserial not null primary key,
 	post_text varchar(150) not null,
 	dateTime timestamp not null,
 	user_id bigint not null references users(id)
-);
+)
+
+create table followers (
+    id bigserial not null primary key,
+    user_id bigint not null references users(id),
+    follower_id bigint not null references users(id)
+)
